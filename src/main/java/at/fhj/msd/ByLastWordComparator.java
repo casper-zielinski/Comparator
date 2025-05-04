0193b584494a7bff78774d9f0cd702b4bf34b14e
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-04 19:30:03 +0200

    changed if statements to return switch statements

--- src/main/java/at/fhj/msd/ByLastWordComparator.java
+++ src/main/java/at/fhj/msd/ByLastWordComparator.java
@@ -8,16 +8,12 @@ public class ByLastWordComparator implements Comparator<String>{
       public int compare(String o1, String o2) {
             String[] a1 = o1.split(" ");
             String[] a2 = o2.split(" ");
-            
-            if (a1[a1.length - 1].compareTo(a2[a2.length -1]) == 1)
-            {
-                  return 1;
-            }
-            else if(a1[a1.length - 1].compareTo(a2[a2.length -1]) == -1)
-            {
-                  return -1;
-            }
-            else return 0;
+
+          return switch (a1[a1.length - 1].compareTo(a2[a2.length -1])) {
+              case 1 -> 1;
+              case -1 -> -1;
+              default -> 0;
+          };
       }
 
 }
