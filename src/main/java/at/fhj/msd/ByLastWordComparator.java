022ab719892b7cdb2d4fae30ca1bf0cb22373d09
Zielinski Casper <casper.zielinski.endercasper@gmail.com>
2025-05-04 19:45:26 +0200

    fixed ByLastWordComparotor for 2nd Example

--- src/main/java/at/fhj/msd/ByLastWordComparator.java
+++ src/main/java/at/fhj/msd/ByLastWordComparator.java
@@ -8,12 +8,24 @@ public class ByLastWordComparator implements Comparator<String>{
       public int compare(String o1, String o2) {
             String[] a1 = o1.split(" ");
             String[] a2 = o2.split(" ");
-
-          return switch (a1[a1.length - 1].compareTo(a2[a2.length -1])) {
-              case 1 -> 1;
-              case -1 -> -1;
-              default -> 0;
-          };
+            
+            if (a1[a1.length - 1].compareTo(a2[a2.length -1]) == 0)
+            {
+                  if (a1.length > a2.length)
+                  {
+                        return 1;
+                  }
+                  if (a1.length < a2.length)
+                  {
+                        return -1;
+                  }
+                  else return 0;
+            }
+            return switch (a1[a1.length - 1].compareTo(a2[a2.length -1])) {
+                  case 1 -> 1;
+                  case -1 -> -1;
+                  default -> 0;
+            };
       }
 
 }
